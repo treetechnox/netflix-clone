7dfe70c4ee56763f25d53319dd85013d84bd9fd9
treetechnox <treetechnox86@gmail.com>
2021-04-07 12:43:56 +0100

    this is my first commit in vs code 23

---
+++ src/Row.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react'
+import axios from './axios';
+//import requests from './requests';
+import './Row.css'
+
+const base_url = "https://image.tmdb.org/t/p/original/"
+
+function Row({ title , fetchUrl}) {
+    const [movies,setMovies] = useState([])
+
+    useEffect(()=>{
+        // if we leave the brackets blank -> [], run once when the row loads 
+        // and don't run again
+
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        } 
+
+        fetchData();
+
+    },[fetchUrl]); 
+
+    console.log(movies);
+
+    return (
+        <div className="row">
+            <h2>{ title }</h2>
+            <div className="row_posters">
+                {movies.map(movie => (
+                    <img
+                    key={movie.id}
+                     className="row__poster"   
+                     src={`${base_url}${movie.poster_path}`} alt={movie.name}></img>     
+                ))}                
+            </div>
+
+            {/* container -> posters */}
+        </div>
+    )
+}
+
+export default Row
